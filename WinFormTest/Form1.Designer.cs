762981267b01ed997ee797f7c73908e4cff88e4f
Juun <65842841+juunie-roh@users.noreply.github.com>
2021-03-19 12:26:16 +0900

    Add files via upload

---
+++ WinFormTest/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace WinFormTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNote = new System.Windows.Forms.TextBox();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnMessage = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNote
+            // 
+            this.txtNote.Location = new System.Drawing.Point(12, 12);
+            this.txtNote.Multiline = true;
+            this.txtNote.Name = "txtNote";
+            this.txtNote.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtNote.Size = new System.Drawing.Size(776, 317);
+            this.txtNote.TabIndex = 0;
+            this.txtNote.Text = "GitHub 동작 Test용 WinForm";
+            this.txtNote.WordWrap = false;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(656, 393);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(132, 45);
+            this.btnSelect.TabIndex = 1;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnMessage
+            // 
+            this.btnMessage.Location = new System.Drawing.Point(518, 393);
+            this.btnMessage.Name = "btnMessage";
+            this.btnMessage.Size = new System.Drawing.Size(132, 45);
+            this.btnMessage.TabIndex = 2;
+            this.btnMessage.Text = "Message";
+            this.btnMessage.UseVisualStyleBackColor = true;
+            this.btnMessage.Click += new System.EventHandler(this.btnMessage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMessage);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.txtNote);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNote;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnMessage;
+    }
+}
+
---
+++ WinFormTest/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMessage_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(txtNote.SelectedText);
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            txtNote.SelectAll();
+            txtNote.Focus();
+        }
+    }
+}
