36f6c6d0de3bf18810c30450af851fe12b9b0db3
곰돌씨 <64863514+tony7078@users.noreply.github.com>
2021-06-02 17:36:21 +0900

    코로나현황 new버젼
    UI 생성

---
+++ WindowsFormsApp1/WindowsFormsApp1/PanelControls/UserControl1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1.PanelControls
+{
+    public partial class UserControl1 : System.Windows.Forms.UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            webBrowser1.Document.Window.ScrollTo(0, 550);
+        }
+    }
+}
