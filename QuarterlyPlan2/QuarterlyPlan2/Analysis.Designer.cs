3e4ecbf94b1eb414a9eb113f99f7a9c09061de52
semin-1432 <65807803+semin-1432@users.noreply.github.com>
2021-06-03 17:11:44 +0900

    코로나 분기별 접종 계획 프로젝트 커밋
    코로나 분기별 접종 순서, 5월 이후 추진 계획, 1분기 접종 분석 결과, 참고 영상

---
+++ QuarterlyPlan2/QuarterlyPlan2/Analysis.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace QuarterlyPlan2
+{
+    partial class Analysis
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(21, 110);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(533, 431);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(17, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(340, 20);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "<1분기 코로나19 예방접종 효과 분석 결과 발표>";
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn1.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn1.Location = new System.Drawing.Point(573, 352);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(103, 33);
+            this.btn1.TabIndex = 25;
+            this.btn1.Text = "Slide 1";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn2.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn2.Location = new System.Drawing.Point(573, 391);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(103, 33);
+            this.btn2.TabIndex = 26;
+            this.btn2.Text = "Slide 2";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn3.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn3.Location = new System.Drawing.Point(573, 430);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(103, 33);
+            this.btn3.TabIndex = 27;
+            this.btn3.Text = "Slide 3";
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn4.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn4.Location = new System.Drawing.Point(573, 469);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(103, 33);
+            this.btn4.TabIndex = 28;
+            this.btn4.Text = "Slide 4";
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn5.Font = new System.Drawing.Font("맑은 고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn5.Location = new System.Drawing.Point(573, 508);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(103, 33);
+            this.btn5.TabIndex = 29;
+            this.btn5.Text = "Slide 5";
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // Analysis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 572);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Analysis";
+            this.Text = "Analysis";
+            this.Load += new System.EventHandler(this.Analysis_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+    }
+}
---
+++ QuarterlyPlan2/QuarterlyPlan2/Analysis.cs
@@ -0,0 +1,57 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuarterlyPlan2
+{
+    public partial class Analysis : MetroForm
+    {
+        public Analysis()
+        {
+            InitializeComponent();
+        }
+
+        private void Analysis_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Load(@"C:\Users\김세민\source\repos\QuarterlyPlan2\QuarterlyPlan2\Analysis1.jpg");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Load(@"C:\Users\김세민\source\repos\QuarterlyPlan2\QuarterlyPlan2\Analysis1.jpg");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Load(@"C:\Users\김세민\source\repos\QuarterlyPlan2\QuarterlyPlan2\Analysis2.jpg");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Load(@"C:\Users\김세민\source\repos\QuarterlyPlan2\QuarterlyPlan2\Analysis3.jpg");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Load(@"C:\Users\김세민\source\repos\QuarterlyPlan2\QuarterlyPlan2\Analysis4.jpg");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Load(@"C:\Users\김세민\source\repos\QuarterlyPlan2\QuarterlyPlan2\Analysis5.jpg");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+    }
+}
